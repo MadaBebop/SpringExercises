6152a167213ce6336d3284e14725901ea2cbd454
MadaBebop <mahdavi.riccardo@gmail.com>
2023-03-11 08:15:31 +0100

    Aggiunta esercizio 1 spring

---
+++ src/main/java/Esercizi/NameController.java
@@ -0,0 +1,21 @@
+package Esercizi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+
+    @GetMapping(value = "/controller/{name}")
+    public String getName(@PathVariable String name) {
+        return name;
+    }
+
+    @PostMapping(value = "/controller/{name}")
+    public String postName(@PathVariable String name) {
+        StringBuilder sb = new StringBuilder(name);
+        return sb.reverse().toString();
+    }
+}
